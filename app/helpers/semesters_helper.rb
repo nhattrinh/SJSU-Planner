c1b904cd4e29a67ec41d14708f79ca2e4f24c0a5
Mohsen Hosseini <mohsen.hosseini@outlook.com>
2016-09-20 21:49:48 -0700

    Revert "Enabled + and - grades in GPA Calculation"
    This reverts commit c4c4e016eb537a8dbc56a6657c79029fd85202a2.

--- app/helpers/semesters_helper.rb
+++ app/helpers/semesters_helper.rb
@@ -3,67 +3,39 @@ module SemestersHelper
     def grade_weight (grade)
         num_grade = case grade
             when "A" then 4.0
-            when "A-" then 3.7
-            when "B+" then 3.29
-            when "B-" then 2.7
             when "B" then 3.0
-            when "C+" then 2.3
             when "C" then 2.0
-            when "C-" then 1.7
-            when "D+" then 1.3
             when "D" then 1.0
-            when "D-"then 0.69
             else 0.0
         end
         return num_grade
     end
 
     def alphabetic_grade (average)
-        if (average == nil)
-            return nil
-        elsif (average == 4.0) 
+        if (average>=4)
             return "A"
-        elsif (average<4 && average>3.7 || average == 3.7) 
-            return "A-"
-        elsif (average< 3.7 && average>3.29 || average == 3.29) 
-            return "B+"
-        elsif (average< 3.29 && average>3.0 || average == 3.0) 
+        elsif (average>=3 && average<4)
             return "B"
-        elsif (average< 3.0 && average>2.7 || average == 2.7) 
-            return "B-"
-        elsif (average<2.7 && average>2.3 || average == 2.3) 
-            return "C+"
-        elsif (average<2.3 && average>2.0 || average == 2.0) 
+        elsif (average>=2 && average<3)
             return "C"
-        elsif (average< 2.0 && average>1.7 || average == 1.7) 
-            return "C-"
-        elsif (average< 1.7 && average>1.3 || average == 1.3) 
-            return "D+"
-        elsif (average< 1.3 && average>1.0 || average == 1.0) 
+        elsif (average>=1 && average<2)
             return "D"
-        elsif (average< 1 && average>0.69 || average == 0.69) 
-            return "D-"
         else 
             return "F"
         end
     end
    
     def grade_point_average (semester)
-        if (!semester.courses.empty?)
-        
-            total = 0
-            credits = 0
-            semester.courses.each do |course|
-                unless course.grade.eql?("") 
-                    credits += course.credits
-                end
-                total += grade_weight(course.grade)*course.credits
+        total = 0
+        credits = 0
+        semester.courses.each do |course|
+            unless course.grade.eql?("") 
+                credits += course.credits
             end
+            total += grade_weight(course.grade)*course.credits
+        end
         total = total/credits
         return total
-        else
-            return nil
-        end
     end
 
         
